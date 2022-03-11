566df1f7a299e8d042be67e37a050e6cacf447b8
thabetchaima <chaima@atayen.us>
2022-03-11 17:31:29 +0100

    test

--- src/pages/AddAffaire.js
+++ src/pages/AddAffaire.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export class AddAffaire extends Component {
   render() {
     return (
-      <div>AddAffaire</div>
+      <div>AddAffaire etst</div>
     )
   }
 }
